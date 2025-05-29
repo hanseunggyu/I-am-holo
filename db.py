b05920806b654a3dc37c2437d5bf0fb3a67b9d97
jiyong <jiyeong627@gmail.com>
2025-05-29 09:49:48 +0900

    last branch 작업

---
+++ db.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+def get_connection():
+    conn = mysql.connector.connect(
+
+        host='3.35.139.92',
+        port=3306,
+        user='root',
+        passwd='secret123',
+        charset='utf8',
+        database='proj-1'
+    )
+    conn.autocommit = True  # 명시적 커밋 자동 설정
+    return conn
+
+if __name__ == "__main__":
+    try:
+        conn = get_connection()
+        print("✅ MySQL 연결 성공!")
+        conn.close()
+    except Exception as e:
+        print("❌ 연결 실패:", e)
